3bc32a4188d5023cbe4a0a4586e77dcc35d00739
Tomasz Cieslar <tomasz.cieslar@capgemini.com>
2019-05-27 21:50:11 +0200

    final solution

--- Other_delegate1/Program.cs
+++ Other_delegate1/Program.cs
@@ -2,11 +2,40 @@
 
 namespace Other_delegate1
 {
+    delegate int ChangeNumber(int i);
     class Program
     {
+
+        static int number = 5;
+
+        public static int AddNumber(int i)
+        {
+            number += i;
+            return number;
+        }
+        public static int MultiplyNumber(int i)
+        {
+            number *= i;
+            return number;
+        }
+        public static int GetNumber()
+        {
+            return number;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ChangeNumber cn1 = new ChangeNumber(AddNumber);
+            ChangeNumber cn2 = new ChangeNumber(MultiplyNumber);
+
+            cn1(5);
+            Console.WriteLine("Wartość liczby: {0}", GetNumber());
+            cn2(10);
+            Console.WriteLine("Wartość liczby: {0}", GetNumber());
+            Console.ReadLine();
+
+
         }
     }
 }
